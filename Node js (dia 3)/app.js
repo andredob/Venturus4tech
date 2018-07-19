4c3a922e780c432d5813158065a34118a2d40b47
unknown <andre-gd@hotmail.com>
2018-07-19 15:40:19 -0300

    desafio adicionado

--- Node js (dia 3)/app.js
+++ Node js (dia 3)/app.js
@@ -20,12 +20,12 @@ const Vaga = require('./model/vaga.js');
     })
 
     //get de uma vapa por ID
-    app.get('/vagas:id', async(req, res) =>{
+    app.get('/vagas/:id', async(req, res) =>{    //find para achar o id do elemento que possui o mesmo id 
         try {
             let id = req.params.id;
-            var vagaId =  vagas.find(el => { el.id == id });
-            if (vagaId != null) return res.send(vagaId);    
-            return res.status(500).send('vagaId=null');
+            let vagasId = vagas.find(el => ( el.id == id));  
+            if (vagasId != null) return res.status(200).send(vagasId)
+            return res.status(500).send('Id inesistente');
         } catch (error) {
             return res.status(500).send('Internal Error');
         }
@@ -35,13 +35,48 @@ const Vaga = require('./model/vaga.js');
     app.post('/vagas', async (req, res) => {
         try {
             let vagasLength = vagas.length;
-            let vaga = createVaga(req.body); //ele passa o body, pois o body ja possui todas as informsções
-            vagas.push(vaga); //adiciona a vaga nova criada no arquivo vagas
+            let vaga = createVaga(req.body);                           //ele passa o body, pois o body ja possui todas as informsções
+            vagas.push(vaga);                                          //adiciona a vaga nova criada no arquivo vagas
             if(vagas.length > vagasLength) return res.send('Added'); /*verifica se o tamanho atual é maior que o tamanho anteriro,
                                                                        se for maior é porque algo foi adicionado e ele deve retornar uma mensagem de sucesso*/
             return res.status(500).send('Internal Error');
         } catch (error) {
             return res.status(500).send('Internal Error');
+        }
+    })
+
+    //put para determinado id
+    app.put('vagas/:id', (req, res) =>{
+        try {
+            if(!req.bod){
+                return res.status(403).send('Adicione algum valor a ser adicionado')
+            }
+
+            let index = vagas.findIndex(job => job.id === req.params.id);
+            if (index >= 0) {
+                Object.keys(req.body).forEach(vaga => {
+                    vagas[index][vaga] = req.body[vaga];
+                })
+                return res.send(`vaga com id ${req.params.id} alterada com sucesso`);
+            }
+            return res.send(`nao foi encontrado esse id`);
+        } catch (error) {
+            return res.send(ìnternal);
+        }
+    })
+
+    //deletar vaga por id
+    app.delete('/vagas/delete:id', async (req, res) => {
+        try {
+            let id = req.params.id;
+            let vagaId = vagas.find(el => el.id == id);
+            if (vagaId != null) {
+                delete vagaId;
+                return res.status(200).send(`Foi deletado a vaga com ID ${vagaId}`)
+            }
+            
+        } catch (error) {
+            
         }
     })
 
@@ -57,3 +92,4 @@ const Vaga = require('./model/vaga.js');
     )
 
 
+   
